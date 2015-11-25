81bf9a08d8e8d08a7c28a6a11f14183ace43a162
Robson Castilho <robsoncastilho@yahoo.com.br>
2015-11-24 21:55:46 -0300

    Camada de infraestrutura Controle transacional com NHibernate

---
+++ NCleanArchitecture.Infrastructure/NHibernateTransaction.cs
@@ -0,0 +1,36 @@
+﻿using NCleanArchitecture.Application._base;
+using NHibernate;
+using System;
+
+namespace NCleanArchitecture.Infrastructure
+{
+    public class NHibernateTransaction : Transactional, IDisposable
+    {
+        private readonly ISession _session;
+
+        public NHibernateTransaction(ISession session)
+        {
+            _session = session;
+        }
+
+        public void BeginTransaction()
+        {
+            _session.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            _session.Transaction.Commit();
+        }
+
+        public void Rollback()
+        {
+            _session.Transaction.Rollback();
+        }
+
+        public void Dispose()
+        {
+            if (_session != null) _session.Dispose();
+        }
+    }
+}
